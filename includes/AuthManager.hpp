d3ee2f21f4f370fead4168571806e20b7e7feead
Abolfazl Azad <abolfazlazad321@gmail.com>
2025-12-25 18:41:40 +0330

    a class for auth for this project

---
+++ includes/AuthManager.hpp
@@ -0,0 +1,27 @@
+
+#ifndef AUTH_MANAGER_HPP
+#define AUTH_MANAGER_HPP
+
+#include "global.hpp"
+#include "User.hpp"
+
+class AuthManager {
+private:
+    vector<User*> users;
+    User* currentUser;
+    
+    User* findUser(string username);
+
+public:
+    AuthManager();
+    ~AuthManager();
+    
+    string signup(string role, string username, string password);
+    string login(string username, string password);
+    string logout();
+    
+    User* getCurrentUser() const { return currentUser; }
+    bool isLoggedIn() const { return currentUser != nullptr; }
+};
+
+#endif
