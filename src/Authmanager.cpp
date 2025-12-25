456b5aab05b6004689eb2ce3b04264594dee1e00
Abolfazl Azad <abolfazlazad321@gmail.com>
2025-12-25 18:43:38 +0330

    functions for auth

---
+++ src/Authmanager.cpp
@@ -0,0 +1,69 @@
+
+#include "AuthManager.hpp"
+
+AuthManager::AuthManager() : currentUser(nullptr) {}
+
+AuthManager::~AuthManager() {
+    for (auto user : users) {
+        delete user;
+    }
+}
+
+User* AuthManager::findUser(string username) {
+    for (auto u : users) {
+        if (u->getUsername() == username)
+            return u;
+    }
+    return nullptr;
+}
+
+string AuthManager::signup(string role, string username, string password) {
+    if (currentUser != nullptr) {
+        return Messages::PERMISSION_DENIED;
+    }
+    
+    if (findUser(username) != nullptr) {
+        return Messages::BAD_REQUEST;
+    }
+    
+    User* newUser = nullptr;
+    if (role == Roles::CHEF) {
+        newUser = new Chef(username, password);
+    } else if (role == Roles::VISITOR) {
+        newUser = new Visitor(username, password);
+    } else {
+        return Messages::BAD_REQUEST;
+    }
+    
+    users.push_back(newUser);
+    currentUser = newUser;
+    return Messages::OK;
+}
+
+string AuthManager::login(string username, string password) {
+    if (currentUser != nullptr) {
+        return Messages::PERMISSION_DENIED;
+    }
+
+    
+    
+    User* user = findUser(username);
+    if(user == nullptr){
+        return Messages::NOT_FOUND;
+    }
+    if(user -> getPassword() != password){
+        return Messages::PERMISSION_DENIED;
+    }
+    
+    currentUser = user;
+    return Messages::OK;
+}
+
+string AuthManager::logout() {
+    if (currentUser == nullptr) {
+        return Messages::PERMISSION_DENIED;
+    }
+    
+    currentUser = nullptr;
+    return Messages::OK;
+}
