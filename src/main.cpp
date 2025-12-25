7d3d87fcf8c5e8a564111b12f53f688a539bf7f7
Abolfazl Azad <abolfazlazad321@gmail.com>
2025-12-25 18:45:51 +0330

    main function for UTCOOK

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,32 @@
-int main() {
+#include "class_recipyfood.hpp"
+#include <iostream>
+
+using namespace std;
+
+string getInput() {
+    string line;
+    if (!getline(cin, line)) return "";
+    return line;
+}
+
+void printOutput(const string& result) {
+    cout << result << endl;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 3) {
+        return 1;
+    }
+    recipyfood system;
+    system.read_file(argv[1], argv[2]);
+    while (true) {
+        string line = getInput();
+        if (line == "") break;
+        
+        string result = system.process(line);
+        printOutput(result);
+    }
+
+
     return 0;
 }
