5b3d3854e6fe63779f545d6d51140671fa1f22cc
Abolfazl Azad <abolfazlazad321@gmail.com>
2025-12-31 21:44:04 +0330

    adding owner and like functions.

--- includes/Recipe.hpp
+++ includes/Recipe.hpp
@@ -2,18 +2,26 @@
 #define RECIPE_HPP
 
 #include "global.hpp"
+#include <unordered_set>
 
 class Recipe {
 private:
     string name;
     vector<pair<string, int>> ingredients;
     vector<string> steps;
+    string owner;
+    int likes;
+    unordered_set<string> likedBy;
 
 public:
     Recipe(const string& name);
 
     void addIngredient(const string& ingredientName, int amount);
     void addStep(const string& step);
+    void setOwner(const string& ownerName);
+    const string& getOwner() const;
+    int getLikes() const;
+    bool addLike(const string& username);
 
     const string& getName() const;
     const vector<pair<string, int>>& getIngredients() const;
