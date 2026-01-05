aa64b45f0a18486b055161c62a66247070d91744
Abolfazl Azad <abolfazlazad321@gmail.com>
2026-01-05 23:21:21 +0330

    add owner like and addlike functions

--- src/recipe.cpp
+++ src/recipe.cpp
@@ -1,6 +1,6 @@
 #include "Recipe.hpp"
 
-Recipe::Recipe(const string& name) : name(name) {}
+Recipe::Recipe(const string& name) : name(name), owner(""), likes(0) {}
 
 void Recipe::addIngredient(const string& ingredientName, int amount) {
     ingredients.push_back({ingredientName, amount});
@@ -8,6 +8,27 @@ void Recipe::addIngredient(const string& ingredientName, int amount) {
 
 void Recipe::addStep(const string& step) {
     steps.push_back(step);
+}
+
+void Recipe::setOwner(const string& ownerName) {
+    owner = ownerName;
+}
+
+const string& Recipe::getOwner() const {
+    return owner;
+}
+
+int Recipe::getLikes() const {
+    return likes;
+}
+
+bool Recipe::addLike(const string& username) {
+    auto result = likedBy.insert(username);
+    if (result.second) {
+        ++likes;
+        return true;
+    }
+    return false;
 }
 
 const string& Recipe::getName() const {
